81d07a22ebc70480c43a56c8ed974f795166bea5
Sweetr-feelin <63345459+Sweetr-feelin@users.noreply.github.com>
2024-04-10 15:59:37 +0400

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,3 +1,40 @@
+4.1.
+class Weapon:
+    def __init__(self, nam, weight, range):
+        self.name = nam # название
+        self.weight = weight # вес
+        self.range = range # дистанция нанесения урона
+    
+    def damage(self, dist):
+        if dist <= self.range:
+            if self.type == "firearms":
+                self.ammo -= 1
+                return print('Огнестрельный урон от оружия', self.name, 'равен', self.firepower)
+            else:
+                return print('Урон холодного оружия', self.name, 'равен', self.sharpness)
+        return 0
+
+class firearms(Weapon):
+    
+    def __init__(self, nam, weight, range, type, ammo, power):
+        super().__init__(nam, weight, range)
+        self.type = "firearms"
+        self.ammo = ammo # количество зарядов
+        self.firepower = power # урон от огнестрела
+
+class cold_weapon(Weapon):
+    
+    def __init__(self, nam, weight, range, type, points):
+        super().__init__(nam, weight, range)
+        self.type = "cold_weapon"
+        self.sharpness = points # урон от хол. оружия
+
+pistol = firearms("Пистолет", 2, 10, firearms, 14, 5)
+pistol.damage(5)
+
+knife = cold_weapon("Нож", 1, 1, cold_weapon, 3)
+knife.damage(1)
+
 4.2. Первая часть задания
 class Warrior: # воин
 
