de4b50f9ac2739fb29ec52d23c42d0ff19d52cfe
Sweetr-feelin <63345459+Sweetr-feelin@users.noreply.github.com>
2024-06-12 14:06:48 +0400

    Update task11_unit_tests.py

--- task11_unit_tests.py
+++ task11_unit_tests.py
@@ -1,3 +1,27 @@
+# Первая версия сортировки:
+def sort_massive(n, min, max):
+    # Ввод данных
+    import random
+    x = []
+    changes = True
+    # Создание списка
+    for i in range(n):
+        x.append(random.randint(min, max))
+    # Определение результата
+    while changes == True:
+        changes = False
+        for j in range(n - 1):
+            if x[j] > x[j + 1]:
+                y = x[j]
+                x[j] = x[j + 1]
+                x[j + 1] = y
+                changes = True
+    # Вывод результата
+    return x
+
+sort_massive(10,-25,25)
+
+# Первая версия тестов:
 import unittest
 from sort_massive import sort_massive
 
@@ -34,6 +58,101 @@ class sort_massive_TestCases(unittest.TestCase):
         x = sort_massive(n, min, max)
         for i in range(n-1):
             self.assertLessEqual(x[i], x[i+1])
+
+if __name__ == '__main__':
+    unittest.main()
+
+# Вторая версия сортировки:
+import random
+
+n = 10
+x = []
+min = 1
+max = 10
+for i in range(n):
+    x.append(random.randint(min, max))
+
+def other_sort_massive(n, x):
+    changes = True
+    while changes == True:
+        changes = False
+        for j in range(n - 1):
+            if x[j] > x[j + 1]:
+                y = x[j]
+                x[j] = x[j + 1]
+                x[j + 1] = y
+                changes = True
+    return x
+
+other_sort_massive(n, x)
+
+# Вторая версия тестов:
+import unittest
+from other_sort_massive import other_sort_massive
+import random
+
+class other_sort_massive_TestCases(unittest.TestCase):
+
+    def test_small_positive(self):  # Малый диапазон положительных целых чисел
+        n = 10
+        min = 1
+        max = 10
+        lst1 = []
+        lst2 = []
+        for i in range(n):
+            x = random.randint(min, max)
+            lst1.append(x)
+            lst2.append(x)
+
+        other_sort_massive(n, lst1)
+        lst2.sort()
+        self.assertEqual(lst1, lst2)
+
+
+    def test_small_negative(self):  # Малый диапазон положительных и отрицательных целых чисел, включая "0"
+        n = 10
+        min = -3
+        max = 3
+        lst1 = []
+        lst2 = []
+        for i in range(n):
+            x = random.randint(min, max)
+            lst1.append(x)
+            lst2.append(x)
+
+        other_sort_massive(n, lst1)
+        lst2.sort()
+        self.assertEqual(lst1, lst2)
+
+    def test_great_negative(self):  # Большой диапазон положительных и отрицательных целых чисел, включая "0"
+        n = 5000
+        min = -10000
+        max = 10000
+        lst1 = []
+        lst2 = []
+        for i in range(n):
+            x = random.randint(min, max)
+            lst1.append(x)
+            lst2.append(x)
+
+        other_sort_massive(n, lst1)
+        lst2.sort()
+        self.assertEqual(lst1, lst2)
+
+    def test_float(self):  # Ввод дробного числа в качестве параметра
+        n = 10.1
+        min = -3
+        max = 3
+        lst1 = []
+        lst2 = []
+        for i in range(n):
+            x = random.randint(min, max)
+            lst1.append(x)
+            lst2.append(x)
+
+        other_sort_massive(n, lst1)
+        lst2.sort()
+        self.assertEqual(lst1, lst2)
 
 if __name__ == '__main__':
     unittest.main()
